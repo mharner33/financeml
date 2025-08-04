314de1647c2a40ec174f8ab0a18d24e9e06b9e9f
Mike Harner <mharner33@gmail.com>
2025-08-03 21:10:17 -0600

    test fmpsdk

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import requests
-from fmpsdk import quote
+import fmpsdk
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+fmp_api_key = os.getenv("FMP_API_KEY")
+print(fmp_api_key)
+
 def main():
-    print("Hello from financeml!")
-    quote.quote("AAPL")
+    # Example: Get historical stock data for Apple
+    try:
+        historical_data = fmpsdk.historical_price_full(apikey=fmp_api_key, symbol='MP')
+        print(historical_data[:5]) # Print the first 5 entries
+    except Exception as e:
+        print(f"An error occurred: {e}")
     
+
 
 if __name__ == "__main__":
     main()
